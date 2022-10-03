3395c75288ab387aeb5954591e61ff82a13192ca
Renvel <anferente10@gmail.com>
2022-10-03 13:20:25 -0500

    connect google auth service

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,48 +1,69 @@
-const {response} = require('express');
-const bcrypt = require('bcryptjs');
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
 
-const User = require ('../models/user');
-const { generateJWT } = require('../helpers/jwt.helper');
-
+const User = require("../models/user");
+const { generateJWT } = require("../helpers/jwt.helper");
+const { googleVerify } = require("../helpers/google-verify.helper");
 
 const login = async (req, res = response) => {
-    const { email, password } = req.body;
-    try {
-        //verify email
-        const userDB = await User.findOne({email});
-        //user mail is not registered
-        if ( !userDB ){ 
-            res.status(404).json({
-                ok:false,
-                msg: 'User not found.'
-            });
-        }
-
-        //verify password
-        const validPassword = bcrypt.compareSync(password, userDB.password);
-        if (!validPassword) {
-            res.status(403).json({
-                ok:false,
-                msg: 'You are not allowed to login.'
-            });
-        }
-
-        const token = await generateJWT(userDB.id);
-        
-        res.json({
-            ok: true,
-            token ,
-        });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok:false,
-            msg: 'Talk with the DBA'
-        });
+  const { email, password } = req.body;
+  try {
+    //verify email
+    const userDB = await User.findOne({ email });
+    //user mail is not registered
+    if (!userDB) {
+      res.status(404).json({
+        ok: false,
+        msg: "User not found.",
+      });
     }
-}
+
+    //verify password
+    const validPassword = bcrypt.compareSync(password, userDB.password);
+    if (!validPassword) {
+      res.status(403).json({
+        ok: false,
+        msg: "You are not allowed to login.",
+      });
+    }
+
+    const token = await generateJWT(userDB.id);
+
+    res.json({
+      ok: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Talk with the DBA",
+    });
+  }
+};
+
+const googleAuth = async (req, res = response) => {
+  const gToken = req.body.token;
+
+  try {
+    const { email, picture, name } = await googleVerify(gToken);
+
+    res.json({
+      ok: true,
+      email,
+      picture,
+      name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Google Authenticartion is incorrect",
+    });
+  }
+};
 
 module.exports = {
-    login
-}
+  login,
+  googleAuth,
+};
---
+++ helpers/google-verify.helper.js
@@ -0,0 +1,16 @@
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_SECRET);
+
+async function googleVerify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_ID,
+  });
+  const payload = ticket.getPayload();
+  console.log(payload);
+  return payload;
+}
+
+module.exports = {
+  googleVerify,
+};
